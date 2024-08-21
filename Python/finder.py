19757e5cf6684c59c719a81555408fbe250d2659
harsh.buddhadev@omen15 <buddhadevharsh953@gmail.com>
2024-08-21 18:22:34 +0530

    Updated and Restructured Files

---
+++ Python/finder.py
@@ -0,0 +1,11 @@
+import re
+count=0
+# pattern=re.compile("python")
+# matcher=pattern.finditer("Afunction python in python is defined by drf in pyhton")
+# print(pattern)
+matcher=re.sub("[0-9]","$","selib98fyvaeughbfp")
+print(matcher)
+# for i in matcher:
+#     count+=1
+#     print(i.start(),"...",i.end(),"...",i.group())
+#     print("the number of occurance:",count)
